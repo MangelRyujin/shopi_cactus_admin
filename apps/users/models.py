7da68c0c699888d952ef95d5363793954bacbb4b
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-03 01:26:09 -0400

    fix: modified REQUIRED_FIELDS in model User

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    
     email = models.EmailField('Correo Electrónico', max_length=255 , unique=True, blank = False, null= False)
     
     class Meta:    
@@ -10,7 +11,7 @@ class User(AbstractUser):
         verbose_name_plural = 'Users'
     
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'name','last_name']
+    REQUIRED_FIELDS = ['email','first_name','last_name']
     
     def natural_key(self):
         return (self.username)
