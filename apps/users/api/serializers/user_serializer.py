4c538b349c4bb362ccf63ce1ce848ebf56dce668
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-03 01:36:22 -0400

    feat: Create user_serializer
    create UserSerializer
    create Password_SetSerializer
    create UpdateUserSerializer

---
+++ apps/users/api/serializers/user_serializer.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from apps.users.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','first_name','last_name','email','username','password')
+        
+    def to_representation(self, instance):
+        return {
+            'id':instance.id,
+            'first_name':instance.name,
+            'last_name':instance.last_name,
+            'username':instance.username,
+            'email':instance.email,
+           
+        }
+    
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    
+        
+class Password_SetSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length =128,min_length =8, write_only = True)
+    password2 = serializers.CharField(max_length =128,min_length =8, write_only = True)
+    
+    def validate(self,data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'password':'Las contraseñas son incorrectas'})
+        return data
+    
+    
+        
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = ('username','email','first_name','last_name')
