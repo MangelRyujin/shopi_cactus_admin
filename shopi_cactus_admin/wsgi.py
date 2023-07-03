61ee1083f3d995577dc718ee863d97778f8ce811
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-03 01:08:39 -0400

    build: Configure manage.py, asgi.py and wsgi.py

--- shopi_cactus_admin/wsgi.py
+++ shopi_cactus_admin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopi_cactus_admin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopi_cactus_admin.settings.local')
 
 application = get_wsgi_application()
