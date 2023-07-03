eda16dc30e79edd3dc13c7b336f88242463831a0
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-03 01:06:41 -0400

    feat: Create model User

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField('Correo Electrónico', max_length=255 , unique=True, blank = False, null= False)
+    
+    class Meta:    
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'name','last_name']
+    
+    def natural_key(self):
+        return (self.username)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name}  {self.last_name}'
