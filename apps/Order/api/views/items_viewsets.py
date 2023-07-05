573f7ce033b3fa80e8a4f79a9db9dd6b1a45ea1c
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-05 16:25:59 -0400

    feat: Create items_viewsets

---
+++ apps/Order/api/views/items_viewsets.py
@@ -0,0 +1,28 @@
+
+from apps.Order.api.serializers.order_serializers import Items_OrderSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class OrderViewSet(viewsets.GenericViewSet):
+    serializer_class = Items_OrderSerializer
+    
+    def get_queryset(self,pk = None):
+        if pk is not None:
+            return self.get_serializer().Meta.model.objects.all()
+        return self.get_serializer().Meta.model.objects.filter(id = pk).first()
+    
+    def list(self,request, *args,**kargs):
+        items = self.get_queryset()
+        if items.exists():
+            items_serializer = self.serializer_class(items, many = True)
+            return Response(items_serializer.data, status=status.HTTP_200_OK)
+        return Response({'message':"Items don't exist"}, status= status.HTTP_404_NOT_FOUND)
+    
+    def retrieve(self, request, pk = None):
+        item = self.get_queryset(pk)
+        if item:
+            item_serializer = self.serializer_class(item)
+            return Response(item_serializer.data, status= status.HTTP_200_OK)
+        return Response({'message':"Item don't exist"}, status= status.HTTP_404_NOT_FOUND)    
--- apps/Order/api/views/order_viewsets.py
+++ apps/Order/api/views/order_viewsets.py
@@ -1,4 +1,4 @@
-from apps.Order.models import Order
+
 from apps.Order.api.serializers.order_serializers import OrderSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -17,11 +17,11 @@ class OrderViewSet(viewsets.GenericViewSet):
         if orders.exists():
             orders_serializers = self.serializer_class(orders, many = True)
             return Response(orders_serializers.data, status=status.HTTP_200_OK)
-        return Response({'message':"Plants don't exist"}, status= status.HTTP_404_NOT_FOUND)
+        return Response({'message':"Orders don't exist"}, status= status.HTTP_404_NOT_FOUND)
     
     def retrieve(self, request, pk = None):
         order = self.get_queryset(pk)
         if order:
             order_serializer = self.serializer_class(order)
             return Response(order_serializer.data, status= status.HTTP_200_OK)
-        return Response({'message':"Plant don't exist"}, status= status.HTTP_404_NOT_FOUND)    
+        return Response({'message':"Order don't exist"}, status= status.HTTP_404_NOT_FOUND)    
