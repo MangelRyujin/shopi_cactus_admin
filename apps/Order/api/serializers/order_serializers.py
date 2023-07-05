0de67d215e8fe4d53cd2c42172f7be27a120dcc9
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-05 15:58:19 -0400

    feat: Create order_serializers.py
    Create OrderSerializer and Item_OrderSerializer

---
+++ apps/Order/api/serializers/order_serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from apps.Order.models import Items_Order, Order
+
+
+class OrderSerializer(serializers.Serializer):
+    class Meta:
+        model = Order
+        fields = '__al__'
+        
+        def to_representation(self, instance):
+            return {
+                'user_id': instance.user_id,
+                'first_name': instance.first_name,
+                'last_name': instance.last_name,
+                'email': instance.email,
+                'cost': instance.cost,
+            }
+            
+        def validate_cost(self,value):
+            if value < 0:
+                raise serializers.ValidationError("El costo debe de ser mayor que 0")
+            return value
+        
+class Items_OrderSerializer(serializers.Serializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        
+        def to_representation(self, instance):
+            return{
+                'plant': instance.plant,
+                'order': instance.order,
+                'cost': instance.cost,
+                'qty': instance.qty,
+            }
+            
+        def validate_cost(self,value):
+            if value < 0:
+                raise serializers.ValidationError("El costo debe de ser mayor que 0")
+            return value   
