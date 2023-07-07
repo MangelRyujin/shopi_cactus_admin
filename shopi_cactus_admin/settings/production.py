645cbe895bd26d5ed14aba9017165cc63b10faa7
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-07 19:15:06 -0400

    build: Configure project for deploy

--- shopi_cactus_admin/settings/production.py
+++ shopi_cactus_admin/settings/production.py
@@ -1,9 +1,14 @@
 from .base import *
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 
@@ -11,16 +16,21 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.squlite3',
+        conn_max_age=600
+    )
 }
 
 
 
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
--- shopi_cactus_admin/urls.py
+++ shopi_cactus_admin/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path,include,re_path
+from django.views.static import serve
 from apps.users.api.views.register_apiview import create_client_api_view
 
 urlpatterns = [
@@ -25,4 +26,11 @@ urlpatterns = [
     path('plants/', include('apps.plants.api.routers')),
     path('orders/', include('apps.Order.api.routers')),
     path('register/', create_client_api_view , name ='register')
+]
+
+
+urlpatterns+=[
+    re_path(r'^media/(?P<path>.*)$',serve, {
+        'document_root': settings.MEDIA_ROOT,
+    })
 ]
