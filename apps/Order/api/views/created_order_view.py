ff5cea54d895b64b3723b62e54332630662d60a5
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-11 02:13:18 -0400

    ref: Modified method create_order_api_view

--- apps/Order/api/views/created_order_view.py
+++ apps/Order/api/views/created_order_view.py
@@ -9,16 +9,18 @@ from apps.Order.api.serializers.order_serializers import OrderSerializer,Items_O
 def create_order_api_view(request):
     if request.method == 'POST':
         order_serializers = OrderSerializer(data = request.data)
+        items = request.data['items']
+        print(items)
         if order_serializers.is_valid():
             order_serializers.save()
-            return Response({'message':'Corectly created order!!!'}, status=status.HTTP_201_CREATED)
+            return Response({'message':'Corectly created order!!!','order_id':order_serializers.data['id']}, status=status.HTTP_201_CREATED)
         return Response({'errors':order_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
     
 # Create items order
-def create_items_order_api_view(request):
-    if request.method == 'POST':
-        items_serializers = Items_OrderSerializer(data = request.data,many = True)
-        if items_serializers.is_valid():
-            items_serializers.save()
-            return Response({'message':'Corectly created items!!!'}, status=status.HTTP_201_CREATED)
-        return Response({'errors':items_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
+# def create_items_order_api_view(request):
+#     if request.method == 'POST':
+#         items_serializers = Items_OrderSerializer(data = request.data,many = True)
+#         if items_serializers.is_valid():
+#             items_serializers.save()
+#             return Response({'message':'Corectly created items!!!'}, status=status.HTTP_201_CREATED)
+#         return Response({'errors':items_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
