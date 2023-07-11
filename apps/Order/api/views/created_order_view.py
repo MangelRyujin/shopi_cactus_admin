63ebb82cee83833bb5c42e76751cf00982407a78
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-11 02:35:26 -0400

    ref: Modified urls

--- apps/Order/api/views/created_order_view.py
+++ apps/Order/api/views/created_order_view.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from apps.Order.api.serializers.order_serializers import OrderSerializer,Items_OrderSerializer
+from apps.Order.api.serializers.order_serializers import OrderSerializer
 
 # Create order
 @api_view(['POST'])
@@ -10,10 +10,9 @@ def create_order_api_view(request):
     if request.method == 'POST':
         order_serializers = OrderSerializer(data = request.data)
         items = request.data['items']
-        print(items)
         if order_serializers.is_valid():
             order_serializers.save()
-            return Response({'message':'Corectly created order!!!','order_id':order_serializers.data['id']}, status=status.HTTP_201_CREATED)
+            return Response({'message':'Corectly created order!!!','order_id':order_serializers.data['id'],'items':items}, status=status.HTTP_201_CREATED)
         return Response({'errors':order_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
     
 # Create items order
