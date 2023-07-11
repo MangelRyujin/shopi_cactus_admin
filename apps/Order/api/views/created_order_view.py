dd1fdf0357d49ac8bf4daaec6c8ed0623ba1f8a1
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-11 00:34:36 -0400

    feat: Add method create_items_order_api_view

--- apps/Order/api/views/created_order_view.py
+++ apps/Order/api/views/created_order_view.py
@@ -2,16 +2,23 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from apps.Order.api.serializers.order_serializers import OrderSerializer
+from apps.Order.api.serializers.order_serializers import OrderSerializer,Items_OrderSerializer
 
 # Create order
 @api_view(['POST'])
 def create_order_api_view(request):
-    
-    
     if request.method == 'POST':
         order_serializers = OrderSerializer(data = request.data)
         if order_serializers.is_valid():
             order_serializers.save()
             return Response({'message':'Corectly created order!!!'}, status=status.HTTP_201_CREATED)
         return Response({'errors':order_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
+    
+# Create items order
+def create_items_order_api_view(request):
+    if request.method == 'POST':
+        items_serializers = Items_OrderSerializer(data = request.data,many = True)
+        if items_serializers.is_valid():
+            items_serializers.save()
+            return Response({'message':'Corectly created items!!!'}, status=status.HTTP_201_CREATED)
+        return Response({'errors':items_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
