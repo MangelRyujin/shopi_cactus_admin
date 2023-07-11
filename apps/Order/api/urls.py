803babb631324610c63010e7d554591f92e8e061
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-11 05:14:27 -0400

    Prueba

--- apps/Order/api/urls.py
+++ apps/Order/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from apps.Order.api.views.created_order_view import create_order_api_view
-# ,create_items_order_api_view
+from apps.Order.api.views.created_order_view import create_order_api_view,create_items_order_api_view
+
 
 urlpatterns = [
     path('order/', create_order_api_view, name ='order'),
-    # path('items_order/', create_items_order_api_view, name ='order'),
+    path('items_order/', create_items_order_api_view, name ='items_order'),
     
 ]
--- apps/Order/api/views/created_order_view.py
+++ apps/Order/api/views/created_order_view.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from apps.Order.api.serializers.order_serializers import OrderSerializer
+from apps.Order.api.serializers.order_serializers import OrderSerializer,Items_OrderSerializer
 
 # Create order
 @api_view(['POST'])
@@ -16,10 +16,14 @@ def create_order_api_view(request):
         return Response({'errors':order_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
     
 # Create items order
-# def create_items_order_api_view(request):
-#     if request.method == 'POST':
-#         items_serializers = Items_OrderSerializer(data = request.data,many = True)
-#         if items_serializers.is_valid():
-#             items_serializers.save()
-#             return Response({'message':'Corectly created items!!!'}, status=status.HTTP_201_CREATED)
-#         return Response({'errors':items_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
+def create_items_order_api_view(request):
+    if request.method == 'POST':
+        items = request.data
+        if items:
+            return Response({'message':items}, status=status.HTTP_200_OK)
+        return Response({'message':'mal'}, status=status.HTTP_400_BAD_REQUEST)
+        # items_serializers = Items_OrderSerializer(data = request.data,many = True)
+        # if items_serializers.is_valid():
+        #     items_serializers.save()
+        #     return Response({'message':'Corectly created items!!!'}, status=status.HTTP_201_CREATED)
+        # return Response({'errors':items_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
