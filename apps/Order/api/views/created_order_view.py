2471bab65af7d9b08c832d8765d4019c36050c63
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-11 06:04:55 -0400

    Prueba

--- apps/Order/api/views/created_order_view.py
+++ apps/Order/api/views/created_order_view.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from apps.Order.api.serializers.order_serializers import OrderSerializer,Items_OrderSerializer
+from apps.Order.models import Items_Order,Order
+from apps.plants.models import Plant
 
 # Create order
 @api_view(['POST'])
@@ -19,7 +21,15 @@ def create_order_api_view(request):
 def create_items_order_api_view(request):
     if request.method == 'POST':
         items = request.data
-        if items:
+        plant = Plant.objects.filter(id = items['plant_id']).first()
+        order = Order.objects.filter(id = items['order_id']).first()
+        item = Items_Order.objects.create(
+            plant = plant,
+            order = order,
+            cost  = items['cost'],
+            qty  = items['qty']
+        )
+        if item:
             return Response({'message':items}, status=status.HTTP_200_OK)
         return Response({'message':'mal'}, status=status.HTTP_400_BAD_REQUEST)
         # items_serializers = Items_OrderSerializer(data = request.data,many = True)
