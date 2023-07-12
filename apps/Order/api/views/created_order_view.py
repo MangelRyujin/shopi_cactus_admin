1a662934ae6b9f62568013214d5c24b5aa5b1aa8
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-07-12 03:51:59 -0400

    fix: modified method create_order_api_view

--- apps/Order/api/views/created_order_view.py
+++ apps/Order/api/views/created_order_view.py
@@ -9,10 +9,9 @@ from apps.Order.api.serializers.order_serializers import OrderSerializer,Items_O
 def create_order_api_view(request):
     if request.method == 'POST':
         order_serializers = OrderSerializer(data = request.data)
-        items = request.data['items']
         if order_serializers.is_valid():
             order_serializers.save()
-            return Response({'message':'Corectly created order!!!','order_id':order_serializers.data['id'],'items':items}, status=status.HTTP_201_CREATED)
+            return Response({'message':'Corectly created order!!!','order_id':order_serializers.data['id']}, status=status.HTTP_201_CREATED)
         return Response({'errors':order_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
     
 # Create items order
